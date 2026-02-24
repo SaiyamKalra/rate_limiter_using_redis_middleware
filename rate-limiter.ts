2706b847d4cd884f57058323d566650f29d2aef2
Saiyam Kalra <kalrasaiyam2006@gmail.com>
2026-02-25 00:38:39 +0530

    [FEAT]:completed the leaky bucket creation

--- rate-limiter.ts
+++ rate-limiter.ts
@@ -1,19 +1,27 @@
 import {NextFunction, Request,Response} from "express";
 import {connectRedis} from './redis';
-const Capacity=15;
-// let count=0;
+const Capacity=10;
 export async function createLeakyBucket(req:Request,res:Response,next:NextFunction){
     try{
+        const time=Date.now();
         const redis=await connectRedis();
         const userId=req.ip;
+        const timeStamp=await redis.lRange(`auth-data:${userId}`,0,-1);
+        for(const t of timeStamp){
+            if(time-Number(t)>=100000){
+                await redis.lPop(`auth-data:${userId}`);
+            }
+            else{
+                break;
+            }
+        }
         const length=await redis.lLen(`auth-data:${userId}`);
-        if(length>Capacity){
+        if(length>=Capacity){
             return res.status(429).json({
                 message:"Too many login attempts."
             });
         }
-        await redis.rpush(`auth-data:${userId}`,Date.now().toString());
-        await redis.lpop(`auth-data:${userId}`);
+        await redis.rPush(`auth-data:${userId}`,time.toString());
         next();
     }
     catch(err){
